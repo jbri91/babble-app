bdc5c8d02c719d931e634ad3bdf5377c0de1b6cc
Johnny Briones <jbrrii91@gmail.com>
2022-12-11 00:33:41 -0500

    Add Footer, Login, Navigation componenets. Set up Navigation bar

---
+++ my-app/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Login from '../components/Login';
+import Navigation from '../components/Navigation';
+import Footer from '../components/Footer';
+
+
+const Home = () => {
+    return(
+        <div>
+            <Navigation />
+            <Login />
+            <Footer />
+        </div>
+    )
+};
+
+
+export default Home;

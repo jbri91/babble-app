e2abfa42ce39e14ef01a4d114ee746e1b054b4a1
Johnny Briones <jbrrii91@gmail.com>
2022-12-11 01:33:36 -0500

    create assets folder

--- my-app/src/components/Navigation.js
+++ my-app/src/components/Navigation.js
@@ -1,7 +1,8 @@
 import React from "react";
-import  AppBar from "@mui/material/Appbar";
+import AppBar from "@mui/material/Appbar";
 import { Toolbar, IconButton, Typography, Button } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
+import Logo from "../assets/icons/logo.png";
 
 
 const Navigation = () => {
@@ -9,19 +10,28 @@ const Navigation = () => {
     <div>
       <AppBar position="static">
         <Toolbar>
-            <IconButton
-            size='large'
-            edge='start'
-            color='inherit'
-            aria-label='menu'
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
             sx={{ mr: 2 }}
-            >
-                <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component='div' sx={{ flexGrow: 1 }}>
-                News
-            </Typography>
-            <Button color="inherit">Login</Button>
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <div className="head-text">
+                <div className="head-image">
+            <img
+              src={Logo}
+              alt="babble logo"
+              style={{ width: 150, height: 100 }}
+            />
+            </div>
+            <p className="logo-image" >Babble</p>
+            </div>
+          </Typography>
+          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
